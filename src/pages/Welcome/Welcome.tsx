88b8ff5a9e981bd800a9c8c812db181707207b51
User23932344 <olegmatusevitch@gmail.com>
2025-12-29 23:26:23 +0300

    started making functionality for HeroPage

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,17 +1,38 @@
+import { useEffect, useState } from "react";
 import styles from "./Welcome.module.scss";
 
 export default function Welcome() {
+  const [placeholder, setPlaceholder] = useState("");
+
+  const [description, setDescription] = useState("");
+
+  
+
+  useEffect(() => {
+    const updatePlaceholder = () => {
+      const hour = new Date().getHours();
+
+      if (hour >= 6 && hour < 12) setPlaceholder("Какие планы на сегодня?");
+      else if (hour >= 12 && hour < 18) setPlaceholder("Что запишем?");
+      else if (hour >= 18 && hour < 23) setPlaceholder("Как прошел твой день?");
+      else setPlaceholder("Всё еще не спишь?");
+    };
+
+    updatePlaceholder();
+
+    const interval = setInterval(updatePlaceholder, 60 * 1000);
+    return () => clearInterval(interval);
+  })
+
   return (
     <div className={styles.wrapper}>
-      <h1 className={styles.title}>Добро пожаловать</h1>
-      <p className={styles.subtitle}>
-        Это стартовая страница приложения.
-      </p>
-
-      <div className={styles.card}>
-        <h2>Что дальше?</h2>
-        <p>Выберите раздел в меню слева, чтобы продолжить.</p>
-      </div>
+      <input
+      className={styles.new}
+        type="text"
+        placeholder={placeholder}
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
     </div>
   );
 }
